b78e6fa80512b415fb81780c227a57548207051b
Maha Sidi <94317341+mahasidi@users.noreply.github.com>
2023-04-02 18:39:12 -0400

    initial commit

--- src/tuiter/tuits/TuitStats.js
+++ src/tuiter/tuits/TuitStats.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {useDispatch} from "react-redux";
 import {updateLikes} from "./tuits-reducer";
+import {updateTuitThunk}
+	from "../../services/tuits-thunks";
 
 const TuitStats = (
 	{
@@ -12,9 +14,11 @@ const TuitStats = (
 			"title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
 			"image": "spacex.jpeg",
 			"liked": false,
+			"disliked": false,
 			"replies": 123,
 			"retuits": 432,
 			"likes": 2345,
+			"dislikes": 890,
 			"handle": "@spacex",
 			"tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
 		}
@@ -22,16 +26,32 @@ const TuitStats = (
 ) => {
 
 	const dispatch = useDispatch();
-	const updateLikesHandler = (id) => {
-		dispatch(updateLikes(id));
-	}
+	const heartElement = document.getElementById(tuit._id);
 
 	return (
 			<div className="row d-inline">
 				<i className="bi bi-chat"></i>{tuit.replies}
 				<i className="bi bi-arrow-repeat heart"></i>{tuit.retuits}
-				<i className="bi bi-heart" id={tuit._id} onClick = {() => updateLikesHandler(tuit._id)}></i>
+				<i onClick={() => dispatch( updateTuitThunk({
+						...tuit,
+						likes: tuit.liked ? tuit.likes - 1 : tuit.likes + 1,
+						liked: !tuit.liked,
+					})
+				)
+				}
+					className={`bi ${tuit.liked ? "bi-heart-fill text-danger" : "bi-heart"}`}
+				></i>
 						 {tuit.likes}
+				<i onClick={() => dispatch( updateTuitThunk({
+						...tuit,
+						dislikes: tuit.disliked ? tuit.dislikes - 1 : tuit.dislikes + 1,
+						disliked: !tuit.disliked,
+					})
+				)
+				}
+				   className={`bi ${tuit.disliked ? "bi-hand-thumbs-down-fill" : "bi-hand-thumbs-down"}`}
+				></i>
+				{tuit.dislikes}
 				<i className="bi bi-share"></i>
 			</div>
 	);
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,5 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import tuits from '../tuits/tuits.json';
+import {updateTuitThunk, createTuitThunk, deleteTuitThunk, findTuitsThunk}
+	from "../../services/tuits-thunks";
+
+const initialState = {
+	tuits: [],
+	loading: false
+}
+
 
 const currentUser = {
 	"userName": "NASA",
@@ -19,47 +27,50 @@ const templateTuit = {
 
 const tuitsSlice = createSlice({
 	name: 'tuits',
-	initialState: tuits,
-	reducers: {
-		createTuit(state, action) {
-			state.unshift({
-				...action.payload,
-				...templateTuit,
-				_id: (new Date()).getTime(),
-			})
-		},
-		deleteTuit(state, action) {
-			const index = state
-				.findIndex(tuit =>
-					tuit._id === action.payload);
-			state.splice(index, 1);
-		},
-		updateLikes(state, action) {
-			const index = state
-				.findIndex(tuit =>
-					tuit._id === action.payload);
-			const tuit = state[index];
+	initialState,
+	extraReducers: {
+		[findTuitsThunk.pending]:
+			(state) => {
+				state.loading = true
+				state.tuits = []
+			},
+		[findTuitsThunk.fulfilled]:
+			(state, { payload }) => {
+				state.loading = false
+				state.tuits = payload
+			},
+		[findTuitsThunk.rejected]:
+			(state, action) => {
+				state.loading = false
+				state.error = action.error
+			},
+		[deleteTuitThunk.fulfilled] :
+			(state, { payload }) => {
+				state.loading = false
+				state.tuits = state.tuits
+					.filter(t => t._id !== payload)
+			},
+		[createTuitThunk.fulfilled]:
+			(state, { payload }) => {
+				state.loading = false
+				state.tuits.push(payload)
+			},
 
-			const heartElement = document.getElementById(tuit._id);
-
-			if (tuit.liked) {
-				tuit.likes--;
-				tuit.liked = false;
-				// make empty heart
-				console.log("you pressed and its been liked");
-				heartElement.className = "bi bi-heart";
-
-			} else {
-				tuit.likes++;
-				tuit.liked = true;
-				// make full heart
-				console.log("you pressed and its not been liked");
-				heartElement.className = "text-danger bi bi-heart-fill";
+		[updateTuitThunk.fulfilled]:
+			(state, { payload }) => {
+				state.loading = false
+				const tuitNdx = state.tuits
+					.findIndex((t) => t._id === payload._id)
+				state.tuits[tuitNdx] = {
+					...state.tuits[tuitNdx],
+					...payload
+				}
 			}
-		}
 
-	}
+	},
+	reducers: { }
 });
+
 
 export const {createTuit, deleteTuit, updateLikes} = tuitsSlice.actions;
 export default tuitsSlice.reducer;
